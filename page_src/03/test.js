f22ce6e61360f110d782a298ff4ee80c69accc2a
Xenaxxx <xuxunxx0129@gmail.com>
2017-02-09 14:17:07 +0800

    Task 03
    1. Array operation in JavaScript
    2. Add DOM elements

---
+++ page_src/03/test.js
@@ -0,0 +1,66 @@
+window.onload = run()
+
+function run(){
+  arrayTest();
+  initDom();
+}
+
+function arrayTest(){
+  var a = [];
+  for (var i = 0; i < 10; i++) {
+    a.push(i);
+    console.log(a);
+  }
+  console.log('The array a is ' + a);
+  for (var i = a.length; i > 0; i--) {
+    console.log('The array length is '+ a.length )
+    a.pop();
+    console.log(i)
+    console.log(a);
+  }
+}
+
+function initDom(){
+  var div1 = document.createElement('div');
+  document.body.appendChild(div1)
+
+  var labelUserName = document.createElement('label');
+  labelUserName.setAttribute('for', 'username');
+  labelUserName.innerHTML = "用户名: "
+  div1.appendChild(labelUserName);
+
+  var userNameInput = document.createElement('input');
+  userNameInput.setAttribute('type', 'text');
+  userNameInput.id = 'username';
+  div1.appendChild(userNameInput);
+
+  var div2 = document.createElement('div');
+  document.body.appendChild(div2);
+
+  var labelPassWord = document.createElement('label');
+  labelPassWord.setAttribute('for', 'password');
+  labelPassWord.innerHTML = "密   码: ";
+  div2.appendChild(labelPassWord);
+
+  var inputPassWord = document.createElement('input');
+  inputPassWord.setAttribute('type', 'password')
+  inputPassWord.id = 'password';
+  div2. appendChild(inputPassWord);
+
+
+  var loginButton = document.createElement('button');
+  loginButton.id = 'submit';
+  loginButton.innerHTML = "登 录"
+  document.body.appendChild(loginButton);
+
+
+  loginButton.addEventListener('click', function() {
+       if(document.getElementById('username').value == ''){
+         alert('请输入用户名');
+       }
+       else if (document.getElementById('password').value == '') {
+         alert('请输入密码')
+       }
+       console.log('正在登录…');
+  });
+}
